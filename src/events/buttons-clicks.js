0a5f8e49aeb49470f5be9afc5118471c5b9c562e
Evgeniy Schemelinin <Kovarniy@users.noreply.github.com>
2020-04-28 21:58:59 +0800

    add option v1

--- src/events/buttons-clicks.js
+++ src/events/buttons-clicks.js
@@ -1,10 +1,30 @@
 let test;
 
+// input parameter - elemet which must be removed
+const removeField = (elemeintId) => {
+  const gameMenu = document.getElementById(elemeintId);
+  gameMenu.remove();
+};
+
+const createSelector = (inEl, nodeName) => {
+  let selector = document.createElement("selector");
+  if (nodeName === "difficultyLvl") {
+    selector.innerHTML = `<option disabled >Easy</option>
+                        <option>Medium</option>
+                        <option>Hard</option>`;
+  }
+  if (nodeName === "cardSet") {
+    selector.innerHTML = `<option disabled >Set1</option>
+                        <option>Set2</option>
+                        <option>Set3</option>`;
+  }
+  inEl.append(selector);
+};
+
 // This btn removed game-menu and uploaded game-field wich cards
 const newGame = () => {
   const doc = document.getElementById("work-space");
-  const gameMenu = document.querySelector(".game-menu");
-  gameMenu.remove();
+  removeField("game-menu");
 
   // TODO: fieldSize - this parametr must be upload from settings.json
   const fieldSize = 12;
@@ -12,11 +32,24 @@ const newGame = () => {
     let div = document.createElement("div");
     div.classList.add("card");
     div.setAttribute("id", `${i}-card`);
-
     div.innerHTML = `<div class="front-card">front</div>
           <div class="back-card">back</div>`;
     doc.append(div);
   }
 };
 
-export { newGame };
+const openOptions = () => {
+  const doc = document.getElementById("work-space");
+  removeField("game-menu");
+
+  createSelector(doc, "difficultyLvl");
+  createSelector(doc, "cardSet");
+
+  let button = document.createElement("button");
+  button.classList.add("menu-btn");
+  button.innerText = "Back";
+  button.setAttribute("id", "back-to-main-menu-btn");
+  doc.append(button);
+};
+
+export { newGame, openOptions };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
-import { newGame } from "./events/buttons-clicks.js";
+import { newGame, openOptions } from "./events/buttons-clicks.js";
 
 window.onload = () => {
   const newGameBtn = document.getElementById("new-game-btn");
+  const optionsBtn = document.getElementById("options-btn");
   newGameBtn.onclick = newGame;
+  optionsBtn.onclick = openOptions;
 };
